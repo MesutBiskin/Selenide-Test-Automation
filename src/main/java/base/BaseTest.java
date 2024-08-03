5bd5be24ef71c4fc3a4df357d3b2a010609c2b73
Mesut Biskin <msbiskin@gmail.com>
2024-08-03 11:21:41 -0400

    add AfterMethod to project

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,6 +2,7 @@ package base;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
@@ -23,5 +24,9 @@ public class BaseTest {
         Configuration.browser = browserName;
         Selenide.open(Url);
 
+    }
+    @AfterMethod
+    public void tearDown(){
+        Selenide.closeWebDriver();
     }
 }
