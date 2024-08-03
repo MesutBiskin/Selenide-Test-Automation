1cbb65a4c766aeaab5e86fb1d70cf5e8367d4b00
Mesut Biskin <msbiskin@gmail.com>
2024-08-03 09:00:03 -0400

    add new test scenarios

--- src/test/java/parabankUserTest.java
+++ src/test/java/parabankUserTest.java
@@ -12,7 +12,7 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class parabankUserTest extends BaseTest {
 
-    @Test
+    @Test(description = "login")
     public void Test001() {
 
         $(By.name("username")).setValue("mbiskin");
@@ -25,7 +25,35 @@ public class parabankUserTest extends BaseTest {
         //$(By.className("error")).shouldHave(Condition.text("The username and password could not be verified."));
        // $(By.className("error")).shouldBe(Condition.visible);
 
+    }
+
+    @Test(description = "Test002")
+    public void Test002() {
+
+        $(By.name("username")).setValue("mbiskin");
+        $(By.name("password")).setValue(("Kgm202608"));
+        $(By.cssSelector("[value='Log In']")).click();
+
+        String errorMessage = $(By.className("error")).getText();
+        System.out.println(errorMessage);
+
+        //$(By.className("error")).shouldHave(Condition.text("The username and password could not be verified."));
+        // $(By.className("error")).shouldBe(Condition.visible);
 
     }
 
+    @Test(description = "unsuccessfull")
+    public void Test003() {
+
+        $(By.name("username")).setValue("mbiskin");
+        $(By.name("password")).setValue(("Kgm202608"));
+        $(By.cssSelector("[value='Log In']")).click();
+
+        String errorMessage = $(By.className("error")).getText();
+        System.out.println(errorMessage);
+
+        //$(By.className("error")).shouldHave(Condition.text("The username and password could not be verified."));
+        // $(By.className("error")).shouldBe(Condition.visible);
+
+    }
 }
