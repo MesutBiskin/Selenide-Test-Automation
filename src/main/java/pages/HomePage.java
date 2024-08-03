f6b031324f6a60ea2e255c8efe7b4cae9bc70527
Mesut Biskin <msbiskin@gmail.com>
2024-08-03 11:30:32 -0400

    added new codes to selenide

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,12 +1,15 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class HomePage {
+
+    @Step("Login Succesfull Page")
     public HomePage succesfullLoginController(String value) {
         $$(By.className("title")).get(0).shouldHave(Condition.text(value));
         return this;
--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,31 +1,38 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
+
+    @Step("User name field fill")
     public LoginPage fillUserName(String text) {
         $(By.name("username")).setValue(text);
         return this;
     }
 
+    @Step("User Password Field Fill")
     public LoginPage fillUserPassword(String text) {
         $(By.name("password")).setValue((text));
         return this;
     }
 
+    @Step("Login Page Click Button")
     public LoginPage clickButton() {
         $(By.cssSelector("[value='Log In']")).click();
         return this;
     }
 
+    @Step("Error message getting")
     public String getErrorMessage() {
         String errorMessage = $(By.className("error")).getText();
         return errorMessage;
     }
 
+    @Step("Error Message Controler Field")
     public LoginPage errorMessageController(String value) {
         $(By.className("error")).shouldHave(Condition.text(value));
         return this;
